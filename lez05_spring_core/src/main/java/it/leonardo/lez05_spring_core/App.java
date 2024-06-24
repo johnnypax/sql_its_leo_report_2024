8cb7c0e994a25df3f37d4d184edd0dfba87a3a04
Giovanni Pace <g.pace@braintechinnovations.it>
2024-06-24 08:45:02 +0200

    Aggiunto esercizio Spring Core

---
+++ lez05_spring_core/src/main/java/it/leonardo/lez05_spring_core/App.java
@@ -0,0 +1,13 @@
+package it.leonardo.lez05_spring_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
